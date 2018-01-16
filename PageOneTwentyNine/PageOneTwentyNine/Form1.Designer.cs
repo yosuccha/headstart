2eeb04edb452f5a950fe652f1b1e8be7b86a8822
Ryan Laffey <laffeyhaha@gmail.com>
2018-01-16 16:30:12 -0600

    this is the cash transfer project from joe to bob

---
+++ PageOneTwentyNine/PageOneTwentyNine/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PageOneTwentyNine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.joesCashLabel = new System.Windows.Forms.Label();
+            this.bobsCashLabel = new System.Windows.Forms.Label();
+            this.bankCashLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // joesCashLabel
+            // 
+            this.joesCashLabel.AutoSize = true;
+            this.joesCashLabel.Location = new System.Drawing.Point(63, 50);
+            this.joesCashLabel.Name = "joesCashLabel";
+            this.joesCashLabel.Size = new System.Drawing.Size(44, 13);
+            this.joesCashLabel.TabIndex = 0;
+            this.joesCashLabel.Text = "Joe has";
+            // 
+            // bobsCashLabel
+            // 
+            this.bobsCashLabel.AutoSize = true;
+            this.bobsCashLabel.Location = new System.Drawing.Point(63, 76);
+            this.bobsCashLabel.Name = "bobsCashLabel";
+            this.bobsCashLabel.Size = new System.Drawing.Size(46, 13);
+            this.bobsCashLabel.TabIndex = 1;
+            this.bobsCashLabel.Text = "Bob has";
+            // 
+            // bankCashLabel
+            // 
+            this.bankCashLabel.AutoSize = true;
+            this.bankCashLabel.Location = new System.Drawing.Point(63, 107);
+            this.bankCashLabel.Name = "bankCashLabel";
+            this.bankCashLabel.Size = new System.Drawing.Size(73, 13);
+            this.bankCashLabel.TabIndex = 2;
+            this.bankCashLabel.Text = "The bank has";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Give $10 to Joe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Receive $5 from Bob";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bankCashLabel);
+            this.Controls.Add(this.bobsCashLabel);
+            this.Controls.Add(this.joesCashLabel);
+            this.Name = "Form1";
+            this.Text = "Fun with Joe and Bob";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label joesCashLabel;
+        private System.Windows.Forms.Label bobsCashLabel;
+        private System.Windows.Forms.Label bankCashLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PageOneTwentyNine/PageOneTwentyNine/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PageOneTwentyNine
+{
+    public partial class Form1 : Form
+    {
+        Guy joe;
+        Guy bob;
+        int bank = 100;
+        public void UpdateForm()
+        {
+            joesCashLabel.Text = joe.Name + " has $" + joe.Cash;
+            bobsCashLabel.Text = bob.Name + " has $" + bob.Cash;
+            bankCashLabel.Text = "The bank has $" + bank;
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            joe = new Guy();
+            joe.Name = "Joe";
+            joe.Cash = 50;
+            
+            bob = new Guy();
+            bob.Name = "Bob";
+            bob.Cash = 100;
+
+            UpdateForm();
+           }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (bank >= 10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("The bank is out of money.");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
+        }
+        
+
+
+    }
+}
