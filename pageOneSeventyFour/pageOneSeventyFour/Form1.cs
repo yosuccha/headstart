ceb4bab6e1937d6c24e953a09a285595ff643611
Ryan Laffey <rlaffy@stratusintelligence.com>
2018-01-19 17:40:42 -0600

    New puzzle 100% a+

---
+++ pageOneSeventyFour/pageOneSeventyFour/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pageOneSeventyFour
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String[] islands = new string[4];
+            islands[0] = "Bermuda";
+            islands[1] = "Fiji";
+            islands[2] = "Azores";
+            islands[3] = "Cozumel";
+
+            string result = "";
+            int refNum;
+            int y = 0;
+
+            int[] index = new int[4];
+            index[0] = 1;
+            index[1] = 3;
+            index[2] = 0;
+            index[3] = 2;
+
+            while (y < 4)
+            {
+                refNum = index[y];
+                result += "\nisland = ";
+                result += islands[refNum];
+
+                y = y + 1;
+            }
+            MessageBox.Show(result);
+        }
+    }
+}
