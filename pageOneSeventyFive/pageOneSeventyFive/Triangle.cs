9252e204532fb7665b1a370ed0db375b19c88db3
Ryan Laffey <rlaffy@stratusintelligence.com>
2018-02-19 09:20:16 -0600

    hmmm

---
+++ pageOneSeventyFive/pageOneSeventyFive/Triangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pageOneSeventyFive
+{
+    class Triangle
+    {
+        double area;
+        int height;
+        int length;
+
+        public static void Main (string[] args)
+        {
+            string results = "";
+            int x = 0;
+
+            //somehting here
+
+            while (x < 5)
+            {
+                Triangle[] ta = new Triangle[4];
+                ta.height = (x + 1) * 2;
+                ta.length = x + 4
+            }
+        }
+    }
+}
---
+++ pageTwoHundred/pageTwoHundred/DinnerParty.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pageTwoHundred
+{
+    class DinnerParty
+    {
+        public const int CostOfFoodPerPerson = 25;
+        public int NumberOfPeople;
+        private int numberOfPeople;
+        decimal CostOfBeveragesPerPerson;
+        decimal CostOfDecorations = 0;
+        
+        
+        public void SetPartyOptions(int people, bool fancy)
+        {
+            numberOfPeople = people;
+            CalculateCostOfDecorations(fancy);
+        }
+        public int GetNumberOfPeople()
+        {
+            return numberOfPeople;
+        }
+        public void SetHealthyOption(bool healthyOption)
+        {
+            if (healthyOption)
+            {
+                CostOfBeveragesPerPerson = (NumberOfPeople * 5.00M);
+            }
+            else
+            {
+                CostOfBeveragesPerPerson = (NumberOfPeople * 20M);
+            }
+        }
+
+        public void CalculateCostOfDecorations(bool fancy)
+        {
+            if (fancy)
+            {
+                CostOfDecorations = (50M + (15.00M * NumberOfPeople));
+            }
+            else
+            {
+                CostOfDecorations = (30M + (7.50M * NumberOfPeople));
+            }
+        }
+
+        public decimal CalculateCost(bool healthyOption)
+        {
+            decimal totalCost = CostOfDecorations + (CostOfBeveragesPerPerson
+                                + CostOfFoodPerPerson) * NumberOfPeople;
+                        if (healthyOption)
+            {
+                return totalCost * .95M;
+            }
+            else
+            {
+                return totalCost;
+            }          
+        }
+    }
+}
