3ede9ba11beeaab0ba256781410a2761009b72e9
Ryan Laffey <rlaffy@stratusintelligence.com>
2018-02-19 12:52:30 -0600

    page272 doesnt work

---
+++ pageTwoSixtySix/WindowsFormsApp1/MySubClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class MySubClass : MyBaseClass
+    {
+        public MySubClass(string baseClassNeedsThis, int anotherValue)
+        {
+            MessageBox.Show("This is the subclass: " + baseClassNeedsThis + " and " + anotherValue)
+                : base(baseClassNeedsThis);
+        }
+    }
+}
